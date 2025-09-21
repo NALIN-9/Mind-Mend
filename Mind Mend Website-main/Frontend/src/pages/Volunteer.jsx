b44239778e6bf041dd33c971aff5156e46f0443a
Nalin  Kumar .k <nalinkantamani@gmail.com>
2025-09-21 22:18:13 +0530

    Update Volunteer.jsx

--- Mind Mend Website-main/Frontend/src/pages/Volunteer.jsx
+++ Mind Mend Website-main/Frontend/src/pages/Volunteer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 // Import your local images
 import nalin from '../images/nalin.jpg';
-import shoib from '../images/shoaib.jpg';
+import shoib from '../images/shoaib.jpg';  // corrected filename
 import raj from '../images/raj.jpg';
 
 const founders = [
